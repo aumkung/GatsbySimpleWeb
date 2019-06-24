2dab642f882a28b300670138ac312077fdd7ad26
tassanai <tassanai.s@mono.co.th>
2019-06-24 16:21:40 +0700

    test embed

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,5 +6,6 @@ export default () => (
     <Layout>
         <h3 className={styles.textCenter}>หน้านี้ยังไม่มีอะไรให้ดู</h3>
         <img className={styles.image} src={images} alt="test" />
+        <iframe width="640" height="360" src="https://www.youtube.com/embed/UbZJNPYWcXc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </Layout>
 )
