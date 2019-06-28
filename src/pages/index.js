cf1f5b8923f059b97341ffbbc7924582f3aedda9
Tassanai <amm_25204@hotmail.com>
2019-06-28 22:38:19 +0700

    add iframe

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from '../components/layout'
 import images from '../assets/images/test.svg'
+import styles from './style.module.scss'
 
 const textCenter = {
     textAlign: 'center'
@@ -12,6 +13,11 @@ export default () => (
             <h3 style={textCenter}>สวัสดีนี่คือ Gatsby Web Framework</h3>
             <p style={textCenter}>ใช้ง่ายอยู่นะ ?</p>
             <img src={images} />
+            <div className={styles.container}>
+                <div className={styles.videoWrapper}>
+                    <iframe src="https://seeme.mthai-demo.com/ch/test-almip/embed/JkP3mK" frameborder="0" allow="encrypted-media" allowfullscreen scrolling="no"></iframe>
+                </div>
+            </div> 
         </div>
     </Layout>
 )
